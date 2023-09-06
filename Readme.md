1b1e99e08ca836341305387d9a2bd2e4b24a1eb5
Amirhossein Alibakhshi <amir.alibakhshi@tapsi.cab>
2023-09-07 01:10:49 +0330

    feat: prefill inputs from URL query parameters

--- Readme.md
+++ Readme.md
@@ -8,6 +8,24 @@ npm run start
 
 your project will be available on http://localhost:3000/trading-calculator
 
+# Reading Inputs from URL
+
+You can prefill the inputs using query parameters. Here is are valid query parameters:
+
+- `asset`
+- `risk`
+- `amount`
+- `entryPrice`
+- `exitPrice`
+- `stopLoss`
+- `lev`
+- `dollar`
+
+Your URL should look like this:
+
+https://saeid4042.github.io/trading-calculator?asset=100&risk=200&amount=300&entryPrice=400&exitPrice=500&stopLoss=600&lev=700&dollar=800
+
+
 # Updating Formulas
 
 in file `src/constants.ts` you can update the formulas in `metrics` variable
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 // @ts-nocheck
-import React, { useState } from 'react';
-import './App.css';
-import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import SouthEastIcon from '@mui/icons-material/SouthEast';
-import NorthEastIcon from '@mui/icons-material/NorthEast';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Inputs from './components/Inputs';
-import Results from './components/Results';
-import { motion } from 'framer-motion';
-import { getAnimationProps } from './constants';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import SouthEastIcon from "@mui/icons-material/SouthEast";
+import NorthEastIcon from "@mui/icons-material/NorthEast";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Inputs from "./components/Inputs";
+import Results from "./components/Results";
+import { motion } from "framer-motion";
+import { getAnimationProps } from "./constants";
 
 export type formProps = {
   amount: number;
@@ -20,38 +20,73 @@ export type formProps = {
   dollar: number;
   asset: number;
   risk: number;
-}
+};
 
 function App() {
   const initialData = {
-    amount: '',
-    entryPrice: '',
-    exitPrice: '',
-    stopLoss: '',
-    lev: '',
-    dollar: '',
-    asset: '',
-    risk: '',
-  }
+    amount: "",
+    entryPrice: "",
+    exitPrice: "",
+    stopLoss: "",
+    lev: "",
+    dollar: "",
+    asset: "",
+    risk: "",
+  };
 
-  const [formData, _setFormData] = useState<formProps>(initialData)
-  const setFormData = (newData) => _setFormData({ ...formData, ...newData});
+  const [formData, _setFormData] = useState<formProps>(initialData);
+  const setFormData = (newData) => _setFormData({ ...formData, ...newData });
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search);  
+    const validParams = [
+      "asset",
+      "risk",
+      "amount",
+      "entryPrice",
+      "exitPrice",
+      "stopLoss",
+      "lev",
+      "dollar",
+    ];
+    const newData = {};
+    for (const [key, value] of queryParams) {
+      if (validParams.includes(key)) {
+        newData[key] = parseFloat(value) || 0;
+      }
+    }
+    setFormData({ ...formData, ...newData });
+  }, []);
+
+
   const calculateMode = () => {
-    if (formData.entryPrice > formData.exitPrice) return <><p>short</p><SouthEastIcon size="small" /></>;
-    if (formData.entryPrice < formData.exitPrice) return <><p>long</p><NorthEastIcon size="small" /></>;
-    return '-';
-  }
+    if (formData.entryPrice > formData.exitPrice)
+      return (
+        <>
+          <p>short</p>
+          <SouthEastIcon size="small" />
+        </>
+      );
+    if (formData.entryPrice < formData.exitPrice)
+      return (
+        <>
+          <p>long</p>
+          <NorthEastIcon size="small" />
+        </>
+      );
+    return "-";
+  };
 
   const calculateModeColor = () => {
-    if (formData.entryPrice > formData.exitPrice) return 'red';
-    if (formData.entryPrice < formData.exitPrice) return 'green';
-    return 'black';
-  }
+    if (formData.entryPrice > formData.exitPrice) return "red";
+    if (formData.entryPrice < formData.exitPrice) return "green";
+    return "black";
+  };
 
   const theme = createTheme({
     palette: {
       black: {
-        main: '#111',
+        main: "#111",
       },
     },
   });
@@ -60,16 +95,33 @@ function App() {
     <ThemeProvider theme={theme}>
       <div className="App">
         <header>
-          <motion.h1 {...getAnimationProps('FADE_FROM_BOTTOM')}>Trade Calculator</motion.h1>
-          <motion.span {...getAnimationProps('FADE_FROM_TOP')} style={{ color:  calculateModeColor() }}><p>Position: </p>{calculateMode()}</motion.span>
+          <motion.h1 {...getAnimationProps("FADE_FROM_BOTTOM")}>
+            Trade Calculator
+          </motion.h1>
+          <motion.span
+            {...getAnimationProps("FADE_FROM_TOP")}
+            style={{ color: calculateModeColor() }}
+          >
+            <p>Position: </p>
+            {calculateMode()}
+          </motion.span>
         </header>
         <main>
-          <Inputs formData={formData} setFormData={setFormData} key={formData} />
+          <Inputs
+            formData={formData}
+            setFormData={setFormData}
+            key={formData}
+          />
           <Results formData={formData} />
         </main>
-        <motion.footer {...getAnimationProps('FADE', 0.5)}>
-          <Button variant='contained' color="error" fullWidth onClick={() => _setFormData(initialData)}>
-            Clear <DeleteIcon size="small" sx={{ ml: '5px' }} />
+        <motion.footer {...getAnimationProps("FADE", 0.5)}>
+          <Button
+            variant="contained"
+            color="error"
+            fullWidth
+            onClick={() => _setFormData(initialData)}
+          >
+            Clear <DeleteIcon size="small" sx={{ ml: "5px" }} />
           </Button>
         </motion.footer>
       </div>
@@ -78,3 +130,4 @@ function App() {
 }
 
 export default App;
+
